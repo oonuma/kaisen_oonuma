7322c49a4872812dd53378a53d20dda146139ab1
oonuma <oonumaster@gmail.com>
2010-10-05 11:04:42 +0900

    rails new kaisen_oonuma

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KaisenOonuma::Application.config.secret_token = '6fe4afc3b80597dc5bb3fb11c5ef1264ee8587fcadabbd833a2476685871e283168412dce5b281e534d5f7f8add5dbd1844493730f9dd0c48c62842fffa32866'
