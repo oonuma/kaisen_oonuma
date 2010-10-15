26b0af6ec2f706d87e393a81aa99887600b06774
oonuma <oonumaster@gmail.com>
2010-10-15 15:56:25 +0900

    add validates_uniqueness_of

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,4 +2,5 @@ class Shop < ActiveRecord::Base
   validates_presence_of :name, :description, :lines_summary
   validates_format_of :image_url, :with => %r{\.(gif|jpg|png)Z}i,
                       :message => "はGIF,JPG,PNG画像のURLでなければなりません"
+  validates_uniqueness_of :name
 end
