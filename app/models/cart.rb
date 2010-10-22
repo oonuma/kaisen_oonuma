f6bc4ae39407afd3209f71200d0993f03c0e03fd
oonuma <oonumaster@gmail.com>
2010-10-22 15:54:39 +0900

    refactor Cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,29 +1,14 @@
 class Cart
-  #attr_reader :items
-
-  #def initialize
-  #  @item_ids = []
-  #end
-
   def add_product(product)
-    quantities_by_item_id[product.id] += 1
-  end
+    current_item = items.find{|item| item.product_id == product.id}
+    if current_item
+      current_item.increment_quantity
+    else
+      items << CartItem.new(product)
+    end
+  end 
 
   def items
-   #Product.find(@item_ids)
-   item_ids.map{|id| Product.find(id)}
-  end
-
-  def items_with_quantity
-   quantities_by_item_id.map do |product_id, quantity|
-     [Product.find(product_id), quantity]
-   end
-  end
-
-  private
-  def quantities_by_item_id
-    @item_ids ||= []
-    @quantities_by_item_id ||= Hash.new(0)
+    @items ||= []
   end
 end
-
