5ef44ee149fcdcd11003a6e13899ecfdeb496a8b
oonuma <oonumaster@gmail.com>
2010-10-22 15:00:17 +0900

    add product times in cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,12 +1,12 @@
 class Cart
   #attr_reader :items
 
-  def initialize
-    @item_ids = []
-  end
+  #def initialize
+  #  @item_ids = []
+  #end
 
   def add_product(product)
-    @item_ids << product.id
+    quantities_by_item_id[product.id] += 1
   end
 
   def items
@@ -14,9 +14,16 @@ class Cart
    item_ids.map{|id| Product.find(id)}
   end
 
+  def items_with_quantity
+   quantities_by_item_id.map do |product_id, quantity|
+     [Product.find(product_id), quantity]
+   end
+  end
+
   private
-  def item_ids
+  def quantities_by_item_id
     @item_ids ||= []
+    @quantities_by_item_id ||= Hash.new(0)
   end
 end
 
